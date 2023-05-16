1b729d3547ccf158c7c3fd4debf2532eaf2ba617
hyrinn <126777014+hyrinn@users.noreply.github.com>
2023-05-16 13:31:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("this is the app title")
 x = st.slider('Select a value')
@@ -10,3 +12,6 @@ st.subheader("this is the subheader")
 st.caption("this is the caption")
 st.code("x=2021")
 st.latex(r'''a+a r ^ 1 + a r^2 + a r^3 ''')
+
+df = pd.DataFrame(np.random.randn(500,20)/[50,50] + [37.76, -122.4], columns = ['lat', 'lon'])
+st.map(df)
