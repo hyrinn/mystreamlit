9f780ab1ae46457aaf26a370a01841cf8a5348f6
hyrinn <126777014+hyrinn@users.noreply.github.com>
2023-05-16 13:38:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ st.caption("this is the caption")
 st.code("x=2021")
 st.latex(r'''a+a r ^ 1 + a r^2 + a r^3 ''')
 
-df = pd.DataFrame(np.random.randn(500,20)/[50,50] + [37.76, -122.4], columns=['lat', 'lon'])
+df = pd.DataFrame(np.random.randn(500,2)/[50,50] + [37.76, -122.4], columns=['lat', 'lon'])
 st.map(df)
